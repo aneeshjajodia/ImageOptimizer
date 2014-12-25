73f8d4a7e977755320034039248eaf9ea3a0cb39
Aneesh Jajodia <me@aneesh.me>
2014-12-25 19:51:10 +0530

    ImageOptimizer
    Optimizing Images under the folder “uploads” at the highest possible
    level.

---
+++ gruntfile.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = function(grunt) {
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+
+      imagemin: {
+        dist: {
+          options: {
+            progressive: true,
+            optimizationLevel: 7
+          },
+          files: [{
+            expand: true,
+            cwd: 'uploads/',
+            src: '**/*.{jpg,jpeg,png}',
+            dest: 'uploads/'
+          }]
+        }
+      },
+
+      smushit: {
+        mygroup: {
+          src: [
+                'uploads/**/*.jpg',
+                'uploads/**/*.png'
+               ],
+          dest: 'uploads/'
+        }
+      }
+
+  });
+
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-smushit');
+
+grunt.registerTask('default', ['imagemin']);
+
+}
